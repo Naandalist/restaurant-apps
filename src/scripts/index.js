1dcbb95580b6899bbbe9278d2d4eb3803333c781
“naandalist” <listiananda.apriliawan@gmail.com>
2023-01-24 11:41:31 +0700

    feat: update mobile first

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,49 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime";
+import "../styles/main.css";
+import "../styles/responsive.css";
 
-console.log('Hello Coders! :)');
+// Fetch local data json
+import("../DATA.json").then(({ default: jsonData }) => {
+  let datas = jsonData["restaurants"];
+  const items = datas.map((data) => {
+    return `
+           
+
+            <div class="card" tabindex="${0}">
+              <img src="${data["pictureId"]}"
+                alt="${data["name"]}" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">${data["name"]}</h5>
+                <p class="card-text">${data["description"]}</p>
+                <p class="card-text">City: <span class="city">${
+                  data["city"]
+                }</span></p>
+                <p class="card-text">Rating: <span class="rating">${
+                  data["rating"]
+                }</span></p>
+              </div>
+            </div>
+        `;
+  });
+
+    document.querySelector("#content-resto").innerHTML = items.join("");
+});
+
+// Menu
+const menu = document.querySelector("#menu");
+const hero = document.querySelector(".hero");
+const main = document.querySelector("main");
+const drawer = document.querySelector("#drawer");
+
+menu.addEventListener("click", (event) => {
+  drawer.classList.toggle("open");
+  event.stopPropagation();
+});
+
+hero.addEventListener("click", () => {
+  drawer.classList.remove("open");
+});
+
+main.addEventListener("click", () => {
+  drawer.classList.remove("open");
+});
